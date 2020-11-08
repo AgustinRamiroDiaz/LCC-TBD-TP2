2a0cd7e84a1ebed18254b3417d4759212c7933fd
Jfarizano <juanignaciofarizano@gmail.com>
2020-11-08 17:35:33 -0300

    xd

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,30 +1,133 @@
-CREATE DATABASE IF NOT EXISTS `Biblioteca`;
+-- Díaz, Agustín. Legajo: D-4094/1
+-- Farizano, Juan Ignacio. Legajo: F-3562/9
+-- Mellino, Natalia. Legajo: M-6686/9
 
-USE `Biblioteca`;
+-- Ejercicio 1
 
-DROP TABLE IF EXISTS `Escribe`;
-DROP TABLE IF EXISTS `Autor`;
-DROP TABLE IF EXISTS `Libro`;
+CREATE DATABASE IF NOT EXISTS Biblioteca;
 
-CREATE TABLE `Autor`(
-    `ID`            INT UNSIGNED    NOT NULL    AUTO_INCREMENT,
-    `Nombre`        VARCHAR(20)     NOT NULL,
-    `Apellido`      VARCHAR(20)     NOT NULL,
-    `Nacionalidad`  VARCHAR(20)     NOT NULL,
-    `Residencia`    VARCHAR(20)     NOT NULL,
-    PRIMARY KEY (`ID`)
+USE Biblioteca;
+
+DROP TABLE IF EXISTS Escribe;
+DROP TABLE IF EXISTS Autor;
+DROP TABLE IF EXISTS Libro;
+
+CREATE TABLE Autor(
+    ID            INT UNSIGNED    NOT NULL    AUTO_INCREMENT,
+    Nombre        VARCHAR(20)     NOT NULL,
+    Apellido      VARCHAR(20)     NOT NULL,
+    Nacionalidad  VARCHAR(20)     NOT NULL,
+    Residencia    VARCHAR(20)     NOT NULL,
+    PRIMARY KEY (ID)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `Libro`(
-    `ISBN`          int (13) UNSIGNED   NOT NULL,
-    `Titulo`        VARCHAR(30)         NOT NULL,
-    `Editorial`     VARCHAR(20)         NOT NULL,
-    `Precio`        INT UNSIGNED        NOT NULL,
-    PRIMARY KEY (`ISBN`)
+CREATE TABLE Libro(
+    ISBN          int (13) UNSIGNED   NOT NULL,
+    Titulo        VARCHAR(100)        NOT NULL,
+    Editorial     VARCHAR(20)         NOT NULL,
+    Precio        DECIMAL(8, 2)       NOT NULL,
+    PRIMARY KEY (ISBN)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `Escribe`(
-    `ID_autor`       INT UNSIGNED   NOT NULL REFERENCES `Autor` (`ID`)  ON DELETE CASCADE ON UPDATE CASCADE,
-    `ISBN_libro`     INT UNSIGNED   NOT NULL REFERENCES `Libro`(`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (`ID_autor`, `ISBN_libro`)
+CREATE TABLE Escribe(
+    ID_autor       INT UNSIGNED   NOT NULL,
+    ISBN_libro     INT UNSIGNED   NOT NULL,
+    Año            INT UNSIGNED   NOT NULL,
+    PRIMARY KEY (ID_Autor, ISBN_libro),
+    FOREIGN KEY (ID_autor) REFERENCES Autor (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ISBN_libro) REFERENCES Libro (ISBN) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Ejercicio 2
+
+CREATE INDEX titulo_libro
+ON Libro (Titulo);
+
+CREATE INDEX apellido_autor
+ON Autor (Apellido);
+
+
+-- Ejercicio 3
+INSERT INTO Autor VALUES(DEFAULT, 'Julio', 'Cortázar', 'Argentina', 'Buenos Aires');
+INSERT INTO Autor VALUES(DEFAULT, 'Abelardo', 'Castillo', 'Argentina', 'Rosario');
+INSERT INTO Autor VALUES(DEFAULT, 'Natalia', 'Mellino', 'Argentina', 'Buenos Aires');
+INSERT INTO Autor VALUES(DEFAULT, 'Agustín', 'Díaz', 'Rusia', 'Moscú');
+INSERT INTO Autor VALUES(DEFAULT, 'Juan Ignacio', 'Farizano', 'Francia', 'París');
+INSERT INTO Autor VALUES(DEFAULT, 'Stephen', 'Kinga', 'Estados Unidos', 'Portland');
+INSERT INTO Autor VALUES(DEFAULT, 'Eduardo', 'Galeano', 'Uruguay', 'Montevideo');
+
+
+INSERT INTO Libro VALUES(420, 'LOTR', 'UNR', 62.73);
+INSERT INTO Libro VALUES(42, 'Farry Potter y el misterio del recursado', 'Salamandra', 500);
+INSERT INTO Libro VALUES(123, 'Cómo leer un libro', 'Anonymus', 999.99);
+INSERT INTO Libro VALUES(1230, 'Rayuela', 'Alfaguara', 350);
+
+
+INSERT INTO Escribe VALUES(5, 420, 1940);
+INSERT INTO Escribe VALUES(3, 42, 2019);
+INSERT INTO Escribe VALUES(4, 123, 1998);
+INSERT INTO Escribe VALUES(1, 1230, 1967);
+
+-- Ejercicio 4
+
+-- a
+UPDATE Autor
+SET
+    Residencia = 'Buenos Aires'
+WHERE 
+    Nombre = 'Abelardo' AND Apellido = 'Castillo';
+
+-- b
+
+UPDATE Libro
+SET
+    Precio = Precio * 1.1
+WHERE
+    Editorial = 'UNR';
+
+-- c
+
+UPDATE Libro
+SET 
+    Precio = Precio * 1.1
+WHERE 
+    Precio > 200 AND ISBN IN
+                          (SELECT ISBN_Libro FROM Escribe 
+                           WHERE ID_autor IN
+                                          (SELECT ID From Autor 
+                                           WHERE Nacionalidad <> 'Argentina'));
+
+UPDATE Libro
+SET 
+    Precio = Precio * 1.2
+WHERE 
+    Precio <= 200 AND ISBN IN
+                          (SELECT ISBN_Libro FROM Escribe 
+                           WHERE ID_autor IN
+                                          (SELECT ID From Autor 
+                                           WHERE Nacionalidad <> 'Argentina'));
+
+
+-- UPDATE Libro
+-- JOIN Escribe ON Libro.ISBN = Escribe.ISBN_libro
+-- JOIN Autor ON Escribe.ID_autor = Autor.ID
+-- SET 
+--     Libro.Precio = Libro.Precio * 1.1
+-- WHERE 
+--     Libro.Precio > 200 AND Autor.Nacionalidad <> 'Argentina'
+
+
+-- UPDATE Libro
+-- JOIN Escribe ON Libro.ISBN = Escribe.ISBN_libro
+-- JOIN Autor ON Escribe.ID_autor = Autor.ID
+-- SET 
+--     Libro.Precio = Libro.Precio * 1.2
+-- WHERE 
+--     Libro.Precio <= 200 AND Autor.Nacionalidad <> 'Argentina'
+
+-- d
+
+DELETE FROM Libro
+WHERE ISBN IN 
+           (SELECT ISBN_libro FROM Escribe
+           WHERE Año = 1998);
---
+++ consultas.sql
@@ -0,0 +1,52 @@
+-- Díaz, Agustín. Legajo: D-4094/1
+-- Farizano, Juan Ignacio. Legajo: F-3562/9
+-- Mellino, Natalia. Legajo: M-6686/9
+
+-- Ejercicio 5
+
+-- a
+
+SELECT nombre FROM Persona
+WHERE Persona.codigo IN
+                     (SELECT Propietario.codigo FROM Propietario);
+
+-- b
+
+SELECT codigo FROM Inmueble
+WHERE precio >= 600000 AND precio <= 700000;
+
+-- c
+
+SELECT nombre FROM Persona
+WHERE Persona.codigo IN
+                     (SELECT Cliente.codigo FROM Cliente
+                      WHERE Cliente.codigo IN
+                                           (SELECT codigo_cliente FROM PrefiereZona
+                                            WHERE nombre_poblacion = "Santa Fe" AND nombre_zona = "Norte"));
+
+-- d
+
+SELECT DISTINCT Persona.nombre FROM Persona
+JOIN Vendedor ON Persona.codigo = Vendedor.codigo
+JOIN Cliente ON Cliente.vendedor = Vendedor.codigo
+JOIN PrefiereZona ON PrefiereZona.codigo_cliente = Cliente.codigo
+WHERE PrefiereZona.nombre_zona = 'Centro' AND PrefiereZona.nombre_poblacion = 'Rosario';
+
+
+-- e
+
+SELECT Zona.nombre_zona, COUNT(Inmueble.precio), AVG(Inmueble.precio) FROM Zona
+JOIN Inmueble ON Zona.nombre_zona = Inmueble.nombre_zona AND Zona.nombre_poblacion = Inmueble.nombre_poblacion
+WHERE Inmueble.nombre_poblacion = 'Rosario'
+GROUP BY Zona.nombre_zona;
+
+-- f
+
+SELECT Persona.nombre From Persona
+JOIN Cliente ON Cliente.codigo = Persona.codigo
+WHERE NOT EXISTS
+    (SELECT * FROM Zona
+     WHERE Zona.nombre_poblacion = "Santa Fe"
+     AND Zona.nombre_zona NOT IN
+                              (SELECT PrefiereZona.nombre_zona FROM PrefiereZona
+                                WHERE PrefiereZona.codigo_cliente = Cliente.codigo AND PrefiereZona.nombre_poblacion = "Santa Fe"));
