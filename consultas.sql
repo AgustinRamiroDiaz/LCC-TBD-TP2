39cab642d537dd263e1be1919d7db7e3811cd032
Agustin Díaz <agustin.ramiro.diaz@gmail.com>
2020-11-19 12:29:25 -0300

    Chau Join

--- consultas.sql
+++ consultas.sql
@@ -26,26 +26,38 @@ WHERE Persona.codigo IN
 
 -- d
 
-SELECT DISTINCT Persona.nombre FROM Persona
-JOIN Vendedor ON Persona.codigo = Vendedor.codigo
-JOIN Cliente ON Cliente.vendedor = Vendedor.codigo
-JOIN PrefiereZona ON PrefiereZona.codigo_cliente = Cliente.codigo
-WHERE PrefiereZona.nombre_zona = 'Centro' AND PrefiereZona.nombre_poblacion = 'Rosario';
+SELECT Persona.nombre FROM Persona, Vendedor, Cliente, PrefiereZona
+WHERE Persona.codigo = Vendedor.codigo
+AND   Cliente.vendedor = Vendedor.codigo
+AND   PrefiereZona.codigo_cliente = Cliente.codigo
+AND   PrefiereZona.nombre_zona = 'Centro' AND PrefiereZona.nombre_poblacion = 'Rosario';
+
+--TODO ELEGIR ENTRE ESTAS 2
+-- CAPAZ SE PUEDE MEJORAR ESTA SACANDO ALGUNOS IN
+SELECT Persona.nombre FROM Persona
+WHERE Persona.codigo IN 
+    (SELECT Vendedor.codigo FROM Vendedor
+     WHERE Vendedor.codigo IN 
+        (SELECT Cliente.codigo FROM  Cliente
+         WHERE Cliente.codigo IN 
+            (SELECT PrefiereZona.codigo_cliente FROM PrefiereZona
+             WHERE nombre_zona = 'Centro' AND nombre_poblacion = 'Rosario')));
 
 -- e
 
-SELECT Zona.nombre_zona, COUNT(Inmueble.precio), AVG(Inmueble.precio) FROM Zona
-JOIN Inmueble ON Zona.nombre_zona = Inmueble.nombre_zona AND Zona.nombre_poblacion = Inmueble.nombre_poblacion
-WHERE Inmueble.nombre_poblacion = 'Rosario'
+SELECT Zona.nombre_zona, COUNT(Inmueble.precio), AVG(Inmueble.precio) FROM Zona, Inmueble
+WHERE Zona.nombre_zona = Inmueble.nombre_zona AND Zona.nombre_poblacion = Inmueble.nombre_poblacion
+AND   Inmueble.nombre_poblacion = 'Rosario'
 GROUP BY Zona.nombre_zona;
 
 -- f
 
-SELECT Persona.nombre From Persona
-JOIN Cliente ON Cliente.codigo = Persona.codigo
-WHERE NOT EXISTS
+SELECT Persona.nombre From Persona, Cliente
+WHERE Cliente.codigo = Persona.codigo
+AND NOT EXISTS
     (SELECT * FROM Zona
      WHERE Zona.nombre_poblacion = "Santa Fe"
      AND Zona.nombre_zona NOT IN
                               (SELECT PrefiereZona.nombre_zona FROM PrefiereZona
-                                WHERE PrefiereZona.codigo_cliente = Cliente.codigo AND PrefiereZona.nombre_poblacion = "Santa Fe"));
+                                WHERE PrefiereZona.codigo_cliente = Cliente.codigo 
+                                AND PrefiereZona.nombre_poblacion = "Santa Fe"));
